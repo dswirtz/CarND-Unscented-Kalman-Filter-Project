d3abb02e4f6b0e9893199cbb726981faf3226a91
Douglas Wirtz <dougwirtz@gmail.com>
2017-05-01 22:52:30 -0400

    Add files via upload

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -24,10 +24,10 @@ UKF::UKF() {
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
   P_ << 1, 0, 0, 0, 0,
-		  0, 1, 0, 0, 0,
-		  0, 0, 1000, 0, 0,
-		  0, 0, 0, 1000, 0,
-		  0, 0, 0, 0, 1;
+		0, 1, 0, 0, 0,
+		0, 0, 1000, 0, 0,
+		0, 0, 0, 1000, 0,
+		0, 0, 0, 0, 1;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
   std_a_ = 0.53;
